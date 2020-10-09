4a1eb2a22f92ae56ae00cab1a4101cb77e4ec99f
oojas <oojasgupta@gmail.com>
2020-10-09 13:53:37 +0530

    added classes

---
+++ lib/Json/Json.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Parsing extends StatefulWidget {
+  @override
+  _ParsingState createState() => _ParsingState();
+}
+
+class _ParsingState extends State<Parsing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 23,
+        title: Text(
+          'Json Parsing',
+          style: TextStyle(
+              fontSize: 20, fontStyle: FontStyle.italic, color: Colors.black87),
+        ),
+        backgroundColor: Colors.amberAccent,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.brown.shade300,
+      body: ,
+    );
+  }
+}
---
+++ lib/Json/modal.dart
@@ -0,0 +1,1 @@
+class json_parsing {}
